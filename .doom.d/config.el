027e717126238e11104fed6990985fbaec61741f
Andreas Knöpfle <andi@bitcrowd.net>
2022-11-04 09:39:41 +0100

    Semi-light weigth

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 
-(setq doom-font (font-spec :family "Fira Code" :size 17)
+(setq doom-font (font-spec :family "Fira Code" :size 17 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 17))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
